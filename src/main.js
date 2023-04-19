52cfa1156797cd5acd46f9d5d580fab4f67079a8
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2023-04-19 17:18:59 +0200

    dwefer

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
 import foo from "virtual:my-plugin:foo";
+import bar from "./bar";
 
 console.log("loading foo ", foo);
+console.log("loading bar ", bar);
 
 if (import.meta.hot) {
   import.meta.hot.accept("virtual:my-plugin:foo", (newFoo) => {
-    console.log(`hmr accepting ${newFoo} "virtual:my-plugin:foo"`);
+    console.log(`hmr accepting ${newFoo.default} from "virtual:my-plugin:foo"`);
   });
 
-  import.meta.hot.accept("/@id/__x00__virtual:my-plugin:foo", (newFoo) => {
-    console.log(`hmr accepting ${newFoo} "/@id/__x00__virtual:my-plugin:foo"`);
+  import.meta.hot.accept("./bar", (newBar) => {
+    console.log(`hmr accepting ${newBar.default} from "./bar"`);
   });
 }
